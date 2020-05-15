b0bf607dcff3f42b138f9d4321db0cfbc3dc820f
Nikita Shetty <nshetty@chapman.edu>
2020-05-15 03:55:59 -0700

    Initial Setup of Maze and Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+    private bool m_FacingRight = true;
+
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && Valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && Valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && Valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && Valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+    }
+
+    bool Valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+
+    // If the input is moving the player right and the player is facing left...
+                if (move > 0 && !m_FacingRight)
+                {
+                    // ... flip the player.
+                    Flip();
+                }
+                // Otherwise if the input is moving the player left and the player is facing right...
+                else if (move< 0 && m_FacingRight)
+                {
+                    // ... flip the player.
+                    Flip();
+                }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
