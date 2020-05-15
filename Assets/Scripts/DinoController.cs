fdd78d988e3755d8fcc084439e6a9b76c2a3a9df
Nikita Shetty <nshetty@chapman.edu>
2020-05-15 15:03:27 -0700

    Updated Dino Path

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class DinoController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform[] waypoints;
+    int current = 0;
+    public bool facingRight = true;
+
+    public float speed = 0.3f;
+    void FixedUpdate()
     {
-        
+        // Waypoint not reached yet? then move closer
+        if (transform.position != waypoints[current].position)
+        {
+            Vector3 dino = transform.position = Vector3.MoveTowards(transform.position,
+                                            waypoints[current].transform.position,
+                                            speed * Time.deltaTime);
+            if (waypoints[current].position.x > transform.position.x && !facingRight)
+            {
+                Flip();
+            }
+            else if (waypoints[current].position.x < transform.position.x && facingRight)
+            {
+                Flip();
+            }
+            GetComponent<Rigidbody2D>().MovePosition(dino);
+        }
+        // Waypoint reached, select next one
+        else
+            current = (current + 1) % waypoints.Length;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Flip()
     {
-        
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+
     }
 }
